a3ead057df87b132f6966e0193361a5579a79773
Lucas Perez <lucas.alejandro.perez@gmail.com>
2022-03-17 10:44:27 -0300

    cambios en los estilos

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -14,7 +14,6 @@ export const ShoppingPage = () => {
         <ProductCard 
             key={ product.id }
             product={ product }
-            className='bg-dark text-white'
             initialValues={{
                 count: 4,
                 maxCount: 10
@@ -24,19 +23,9 @@ export const ShoppingPage = () => {
             {
                 ( { reset, count, isMaxCountReached, maxCount, increaseBy } ) => (
                     <>
-                        <ProductImage className="custom-image" style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-                        <ProductTitle className="text-bold" />
-                        <ProductButtons className="custom-buttons" />
-
-                        <button onClick={ reset }>
-                            Reset
-                        </button>
-                        <button onClick={ () => increaseBy(-2) }>-2</button>
-                        {
-                            ( !isMaxCountReached && <button onClick={ () => increaseBy(+2) }>+2</button> )
-                        }
-                        
-                        <span>{ count } - { maxCount }</span>
+                        <ProductImage />
+                        <ProductTitle />
+                        <ProductButtons />
                     </>
                 )
             }
